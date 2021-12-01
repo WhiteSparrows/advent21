c2d80da1980bee94ae7bc36e8cab5cf5c9291d7e
Nicolas Cavaillès--Wurmser <nicolas.cavailles--wurmser@grenoble-inp.org>
2021-12-01 18:29:45 +0100

    useless lines erased

--- 1day/main.py
+++ 1day/main.py
@@ -3,7 +3,6 @@ import os
 
 filename = __file__[0:-7] + "/data1.txt"
 
-
 #Main loop
 with open(filename, "r") as f:
     data = f.read()
@@ -15,4 +14,3 @@ print("first result is : ", d)
 
 a = sum(1 for i in range(1, len(measurements))if sum(measurements[i : i + 3]) > sum(measurements[i - 1 : i + 2]))
 print("second result is : ", a)
-
